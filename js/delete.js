d6173117279cdc17b197d3e7fc43068557b11a3b
Gabriel Matos <business.ginvest@gmail.com>
2019-12-16 10:24:29 -0200

    removido delete de produtos

--- js/delete.js
+++ js/delete.js
@@ -1,12 +1,3 @@
-function delProdCarrinho(idItem){
-	parseInt(idItem);
-	db.transaction((tx)=>{
-		tx.executeSql('DELETE FROM produto WHERE id='+idItem);
-	}, errorDB);
-
-	produto_view();
-}
-
 function deleteItemCarrinho(idItem){
 	db.transaction((tx)=>{
 		tx.executeSql('DELETE FROM carrinho WHERE idProduto='+idItem+'');
